c48e666890d2e3652512a9637ecdb7118e9165b1
Naderezzat <75006578+Naderezzat@users.noreply.github.com>
2020-11-25 15:12:25 -0800

    SCHOOL

---
+++ footer.php
@@ -0,0 +1,9 @@
+<section class="fotteroption">
+	 <h2>&copy; <?php echo date("Y"); ?>  YAT School </h2>
+		<p><?php echo "" ?></p>
+	</section>
+
+
+</div>
+</body>
+</html>
---
+++ master.php
@@ -0,0 +1,54 @@
+
+
+<!DOCTYPE html>
+<html>
+<head>
+	<title>YAT School</title>
+
+<!--Front View Template Css file Start-->
+<style type="text/css">
+	   
+    .body{font-family:<?php echo $fonts;?>}
+    .phpcoding{width:900px; margin: 0 auto; background:#ddd;}
+    .headeroption, .fotteroption{background:#444; color: #fff; text-align:center;padding:20px;}
+    .headeroption h2, .fotteroption h2 {margin: 0}
+
+    .maincontent {min-height:400px; padding:20px;}
+.error {color: #FF0000;}
+p{margin: 0}
+.tblone{width:100%;border:1px; solid:#fff; margin:20px 0}
+.tblone td{padding:5px 10px; text-align: center;}
+table.tblone tr:nth-child(2n+1){background:#fff;height:30px;}
+table.tblone tr:nth-child(2n){background:#ffe;height:30px;}
+
+</style>
+
+  <link rel="stylesheet" href="css/styleshite1.css">
+
+</head>
+<!--Front View Template Css file -->
+<body>
+
+
+
+<div class="phpcoding">
+	<section class="headeroption">
+		<h2><?php echo "" ?></h2>
+  <ul class="b">
+ 
+  
+  <h3 style="color:white;" align="center"> Registration Course  </h3> 
+
+
+
+ 
+
+  
+  </ul>
+	</section>
+
+	</div>
+</body>
+</html>
+
+
